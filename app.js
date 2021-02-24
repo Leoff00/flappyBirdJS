d1056f7d31939c2a31be63d6f29efa2a90341805
Leoff00 <leof0180@gmail.com>
2021-02-24 02:56:56 -0300

    creating a localhost to application

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express()
 
 app.use(express.static(__dirname + '/public/'))
 
-app.listen(8081, () => {
-    console.log('Listening on PORT: 3333');
+app.listen(8080, () => {
+    console.log('Listening on PORT: 3333, http://localhost:3333');
 })
